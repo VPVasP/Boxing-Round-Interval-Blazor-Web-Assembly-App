4eb2ff31fea3ec8d87255bf0d33d0572c7b5ada0
Vp <vasilhspinis@gmail.com>
2024-02-22 22:08:09 +0200

    Initial Commit

---
+++ BoxingRoundIntervalWeb/Program.cs
@@ -0,0 +1,11 @@
+using BoxingRoundIntervalWeb;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
